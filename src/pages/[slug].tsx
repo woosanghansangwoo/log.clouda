385c81743362be45657f2b72756ab71de7e38727
root <root@blog.clouda.cloud>
2024-02-16 00:07:10 +0900

    last

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -54,7 +54,7 @@ export const getStaticProps: GetStaticProps = async (context) => {
 const DetailPage: NextPageWithLayout = () => {
   const post = usePostQuery()
 
-  if (!post) return <CustomError />
+  if (!post) return <CustomError /> 
 
   const image =
     post.thumbnail ??
